2288fdf1a7b2a41ec12493954f426d75cf81e510
Joao C Vieira <joaocv3@gmail.com>
2019-09-21 16:33:28 -0300

    WIP

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,16 @@
 class Student < ApplicationRecord
+  MAX = 5
   belongs_to :team
   belongs_to :course
+
+  validate :team_size
+
+  private
+
+  def team_size
+    if Student.where(team: team).size == MAX
+      errors.add(:team, "Team is full!")
+    end
+  end
+
 end
