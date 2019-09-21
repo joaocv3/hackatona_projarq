bf2d0c110bb977e0737758efa9fdafefa62580ee
henriquekops <henriquekops@gmail.com>
2019-09-21 17:54:03 -0300

    fix appraisal amount validation

--- app/models/appraisal.rb
+++ app/models/appraisal.rb
@@ -7,20 +7,20 @@ class Appraisal < ApplicationRecord
   private
 
   def appraisal_amount
-    if self.working_software < 0 or self.working_software > 5
+    if !self.working_software or self.working_software < 0 or self.working_software > 5
       errors.add(:working_software, "has invalid amount!")
     end
-    if self.process < 0 or self.process > 5
+    if !self.process or self.process < 0 or self.process > 5
       errors.add(:process, "has invalid amount!")
     end
-    if self.pitch < 0 or self.pitch > 5
+    if !self.pitch or self.pitch < 0 or self.pitch > 5
       errors.add(:pitch, "has invalid amount!")
     end
-    if self.inovation < 0 or self.inovation > 5
+    if !self.inovation or self.inovation < 0 or self.inovation > 5
       errors.add(:inovation, "has invalid amount!")
     end
-    if self.team_formation < 0 or self.team_formation > 5
-      errors.add(:inovation, "has invalid amount!")
+    if !self.team_formation or self.team_formation < 0 or self.team_formation > 5
+      errors.add(:team_formation, "has invalid amount!")
     end
   end
 
