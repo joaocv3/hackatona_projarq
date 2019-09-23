605c6ee79001d2adce306c9537d84769e5ee3497
Joao C Vieira <joaocv3@gmail.com>
2019-09-23 14:37:17 -0300

    feature: Add navigation header

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :appraisers
   resources :teams
   resources :courses
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "appraisals#index"
 end
