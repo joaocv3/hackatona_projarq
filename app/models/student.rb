1f5710133849bb5d8660bc6684a43d282178c7bb
Joao C Vieira <joaocv3@gmail.com>
2019-09-23 15:46:18 -0300

    feature: Add validation for teams with students for different courses

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,13 +4,21 @@ class Student < ApplicationRecord
   belongs_to :course
 
   validate :team_size
+  validate :team_courses
 
   private
 
   def team_size
-    if Student.where(team: team).size == MAX
+    if team.students.size == MAX
       errors.add(:team, "is full!")
     end
   end
 
+  def team_courses
+    if (team.students.size == MAX - 1) && (team.students.map(&:course).uniq.size == 1)
+      if team.students.sample.course == course
+        errors.add(:team, "needs students from at least 2 different courses!")
+      end
+    end
+  end
 end
