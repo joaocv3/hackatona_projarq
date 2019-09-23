f7b0d2221c0d86d501251c53a16fd657adc2105b
Joao C Vieira <joaocv3@gmail.com>
2019-09-23 16:15:50 -0300

    feature: Refactor team with different courses validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,24 +1,33 @@
 class Student < ApplicationRecord
-  MAX = 5
   belongs_to :team
   belongs_to :course
 
   validate :team_size
-  validate :team_courses
+  validate :team_with_different_courses
 
   private
 
   def team_size
-    if team.students.size == MAX
+    if team.students.size == Constants::MAX_STUDENTS_PER_TEAM
       errors.add(:team, "is full!")
     end
   end
 
-  def team_courses
-    if (team.students.size == MAX - 1) && (team.students.map(&:course).uniq.size == 1)
-      if team.students.sample.course == course
-        errors.add(:team, "needs students from at least 2 different courses!")
-      end
+  def team_with_different_courses
+    if team_almost_full? && team_missing_students_from_different_courses? && new_student_course_already_in_team?
+      errors.add(:team, "needs students from at least #{Constants::DIFFERENT_COURSES_PER_TEAM} different courses!")
     end
+  end
+
+  def team_almost_full?
+    team.students.size == Constants::MAX_STUDENTS_PER_TEAM - ( Constants::DIFFERENT_COURSES_PER_TEAM - 1 )
+  end
+
+  def team_missing_students_from_different_courses?
+    team.students.map(&:course).uniq.size == ( Constants::DIFFERENT_COURSES_PER_TEAM - 1 )
+  end
+
+  def new_student_course_already_in_team?
+    team.students.map(&:course).include?(course)
   end
 end
